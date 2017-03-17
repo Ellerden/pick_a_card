6bc367d23c5a9711807f80a5e889562900d4eb5f
Daria Shcheglova <ellerden@gmail.com>
2017-03-17 20:27:25 +0300

    added unicode symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
-suits = ["Spades", "Hearts","Diamonds", "Clubs"]
+suits = ["\u{2660}", "\u{2665}","\u{2666}", "\u{2663}"]
 puts "#{values.sample} of #{suits.sample}"
